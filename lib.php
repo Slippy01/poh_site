1a85c0da198db363b281c4dcad626e1d83718b4d
Slippy01 <t.stepan774@yandex.ru>
2016-12-06 01:17:01 +0400

    Исправляю кодировку
    Пробую cp1251 (в скрипте и БД)

--- lib.php
+++ lib.php
@@ -14,7 +14,7 @@ function makeOpisan()
 	if($db){
 		if(mysql_select_db(DbName,$db))
 		{
-			mysql_query("SET names 'windows-1251'",$db);
+			mysql_query("SET names 'cp1251'",$db);
 			
 			$rez = mysql_query("SELECT * FROM opisan",$db);
 			
